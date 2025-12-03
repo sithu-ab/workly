ad0ecd8ee5759b68ec84d41d1a6c6b78a294fea4
Sithu <sithu@aluto-benli.com>
2025-12-03 18:09:50 +0630

    Init repo using start.spring.io

---
+++ src/main/java/app/demo/workly/WorklyApplication.java
@@ -0,0 +1,13 @@
+package app.demo.workly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/demo/workly/WorklyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.demo.workly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
