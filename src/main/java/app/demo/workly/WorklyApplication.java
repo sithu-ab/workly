75e598b5ff5ac88fc12f162b3d8a8b1429772830
Sithu <sithu@aluto-benli.com>
2025-12-03 18:24:41 +0630

    Add home page request

--- src/main/java/app/demo/workly/WorklyApplication.java
+++ src/main/java/app/demo/workly/WorklyApplication.java
@@ -2,9 +2,17 @@ package app.demo.workly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WorklyApplication {
+
+    @GetMapping("/")
+    String home() {
+        return "Hello Sprint Boot!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WorklyApplication.class, args);
