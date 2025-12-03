7f1732a51e5d0a0c42febd4eccfb43b933451a21
Sithu <sithu@aluto-benli.com>
2025-12-03 18:38:58 +0630

    Add health check API

---
+++ src/main/java/app/demo/workly/api/HealthCheckController.java
@@ -0,0 +1,14 @@
+package app.demo.workly.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthCheck")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+}
